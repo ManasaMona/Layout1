1e32b2189b2db5f11acd3375c73e103f8c4ada88
ManasaMona <manasaalisetty@gmail.com>
2017-02-09 15:38:13 +0530

    version 2

--- app/src/main/java/com/example/manasaa/task1/MainActivity.java
+++ app/src/main/java/com/example/manasaa/task1/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static com.example.manasaa.task1.R.mipmap.ic_launcher;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG= MainActivity.class.getSimpleName();
     Button button1,button2,button3;
@@ -14,8 +16,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"******onCreate8");
+        Log.d(TAG,"*ON-CREATE*");
         setContentView(R.layout.activity_main);
+
+
         button1=(Button)findViewById(R.id.button1);
         button2=(Button)findViewById(R.id.button2);
         button3=(Button)findViewById(R.id.button3);
@@ -26,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 textView.setText("This is button 1");
                 textView.setVisibility(View.VISIBLE);
+                button1.setEnabled(false);
+                button2.setEnabled(true);
+                button3.setEnabled(true);
             }
         });
 
@@ -34,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 textView.setText("This is button 2");
                 textView.setVisibility(View.VISIBLE);
+                button1.setEnabled(true);
+                button2.setEnabled(false);
+                button3.setEnabled(true);
             }
         });
 
@@ -42,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 textView.setText("This is button 3");
                 textView.setVisibility(View.VISIBLE);
+                button1.setEnabled(true);
+                button2.setEnabled(true);
+                button3.setEnabled(false);
             }
         });
     }
@@ -62,6 +75,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG,"*ONRESUME*");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"*ONSTOP*");
     }
 
     @Override
